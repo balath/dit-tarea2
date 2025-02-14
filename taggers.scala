34a311856ee0b8692fb0f27dc1ae2760e40dded7
Sergio Flor <sflor5@alumno.uned.es>
2025-02-14 19:33:23 +0100

    Add files via upload

---
+++ taggers.scala
@@ -0,0 +1,36 @@
+import scala.io.Source
+
+object LexicalTagComparison extends App:
+  val expectedTags = readTags("src/main/scala/taggedUniversal.txt")
+  val obtainedTags = readTags("src/main/scala/stanzaOutput.txt")
+  val (errorsMap, matches) = compare(expectedTags, obtainedTags)
+  println(s"\nAciertos: $matches de ${expectedTags.length}")
+  println(s"Precisión: ${(matches.toDouble / expectedTags.length) * 100}")
+  println(errorsMap.toSeq.sortBy(_._1).mkString("\n"))
+
+  type tagList = Seq[(String, String)]
+
+  def compare(expected:tagList, obtained:tagList):(Map[String,Int], Int) =
+    val errorList = expected
+      .zip(obtained)
+      .map((tag1, tag2) =>
+        if tag1._2 == tag2._2 then None
+        else Some(s"Word: ${tag1._1} Expected: ${tag1._2} \tObtained: ${tag2._2}")
+      )
+    val errorOcurrencesByKind = errorList
+      .filter(_.isDefined)
+      .map(_.get)
+      .groupMapReduce(s => s)(_ => 1)(_ + _)
+    (errorOcurrencesByKind, errorList.count(_.isEmpty))
+
+  def readTags(filename: String): tagList =
+    val textFile = Source.fromFile(filename)
+    val tags = textFile
+      .getLines()
+      .map(line => {
+        val elements = line.split(" ")
+        (elements(0).toLowerCase(), elements(1).toLowerCase())
+      })
+      .toSeq
+    textFile.close()
+    tags
